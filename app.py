f077dbb5329b58567c8e7f88d3b3e26c199854e9
dsc530 <66384040+dsc530@users.noreply.github.com>
2024-09-20 10:17:57 -0500

    Update app.py
    Uso de checkbox

--- app.py
+++ app.py
@@ -4,17 +4,22 @@ import plotly_express as px
 
 car_data=pd.read_csv('vehicles_us.csv')
 
-st.header('Histograma de Datos')
-hist_button=st.button('Construir histograma')
-scat_button=st.button('Construir dispersión')
+st.header('Datos de Autos Disponibles Para Venta')
 
-if hist_button:
+st.subheader('Por favor escoja como desea observar los datos:')
+
+hist_chart=st.checkbox('Histograma')
+scat_chart=st.checkbox('Gráfico de dispersión')
+
+
+if hist_chart:
     
     st.write('Creación de un histograma para los datos de anuncio de coches')
-    fig=px.histogram(car_data,x='odometer')
-    st.plotly_chart(fig, use_container_width=True)
+    fig=px.histogram(car_data,x='odometer',title='Cantidad de Kms Recorridos Por Auto',labels = {'odometer': 'Kms Recorridos'})
+    st.plotly_chart(fig, use_container_width=True,)
+    
 
-if scat_button:
+if scat_chart:
     st.write('Creacion de un gráfico de dispersión para los datos de anuncio de coches')
-    fig=px.scatter(car_data, x="odometer", y="price")
-    st.plotly_chart(fig, use_container_width=True)
+    fig=px.scatter(car_data, x="odometer", y="price", title='Precio de Acuerdo a los Kms Recorridos',labels = {'odometer': 'Kms Recorridos', 'price':'Precio Vehículo/USD'})
+    st.plotly_chart(fig, use_container_width=True,)
